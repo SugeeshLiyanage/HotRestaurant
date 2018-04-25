2cd7f81d33437bfda9e43cba7b926395619930be
sugeesh <sugeeshdeveloper@gmail.com>
2018-04-24 19:53:17 -0500

    server.js added

---
+++ app/data/customers.js
@@ -0,0 +1,18 @@
+var customers = [
+    {
+      name: "Ross Geller",
+      phone: "512-210-6232",
+      email: "sug@gmail.com",
+      uniqueid: "001"
+    },
+    {
+        name: "dil",
+        phone: "111-222-1234",
+        email: "sug@gmail.com",
+        uniqueid: "002"
+      }
+    
+    ];
+   
+  // export the array
+  module.exports = customers;
